c04664f4e8b7f845d826f4246fa5f2bae0744401
Christian <82107238+christian45410@users.noreply.github.com>
2021-04-29 09:08:44 -0500

    Update Flush Remote DNS.ps1

--- Flush Remote DNS.ps1
+++ Flush Remote DNS.ps1
@@ -1,11 +1,9 @@
-﻿$listofservers = Get-Content .\servers.txt
+$listofservers = Get-Content .\servers.txt
 
-foreach ($servers in $listofservers) {
-
+foreach ($servers in $listofservers) 
+{
 Invoke-WmiMethod -class Win32_process -name Create -ArgumentList ("cmd.exe /c ipconfig /flushdns") -ComputerName $servers
-
 Invoke-WmiMethod -class Win32_process -name Create -ArgumentList ("cmd.exe /c ipconfig /registerdns") -ComputerName $servers
-
 }
 
 pause
