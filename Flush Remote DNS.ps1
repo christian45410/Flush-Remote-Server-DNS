12143d20c51492a8e16a2be6b8c49d0ef5fd880c
Christian <82107238+christian45410@users.noreply.github.com>
2021-04-29 09:07:42 -0500

    Add files via upload

---
+++ Flush Remote DNS.ps1
@@ -0,0 +1,11 @@
+﻿$listofservers = Get-Content .\servers.txt
+
+foreach ($servers in $listofservers) {
+
+Invoke-WmiMethod -class Win32_process -name Create -ArgumentList ("cmd.exe /c ipconfig /flushdns") -ComputerName $servers
+
+Invoke-WmiMethod -class Win32_process -name Create -ArgumentList ("cmd.exe /c ipconfig /registerdns") -ComputerName $servers
+
+}
+
+pause
